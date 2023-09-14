9a9eb071d191244dab388af2b348100e9d44c195
Ole Tytlandsvik <ole.tytlandsvik@gmail.com>
2023-09-14 20:58:18 +0200

    initial commit

---
+++ CLI/Program.fs
@@ -0,0 +1,19 @@
+﻿module CLI
+
+open FsHttp
+
+let testGet (url: string) =
+    http {
+        GET url
+    }
+    |> Request.send
+    |> Response.toJson
+    //|> fun json -> printf $"{json.ToString}"
+
+[<EntryPoint>]
+let main args =
+    let url: string = "http://localhost:8080/messages"
+    
+    printfn $"{testGet url}"
+    // Return 0
+    0
